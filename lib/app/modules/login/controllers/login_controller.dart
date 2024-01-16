07685730dcd1077cc3b9ca7893399cfecab570ea
YohAns86 <yohananangekap@gmail.com>
2024-01-16 14:48:54 +0700

    login page

---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,65 @@
+import 'package:dio/dio.dart' as dio;
+import 'package:fluter_yohan33/app/routes/app_pages.dart';
+import 'package:fluter_yohan33/constant/endpoint.dart';
+import 'package:fluter_yohan33/provider/api_provider.dart';
+import 'package:fluter_yohan33/provider/storage_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  //TODO: Implement LoginController
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final count = 0.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+
+  final loadingLogin = false.obs;
+
+  login() async {
+    loadingLogin(true);
+    try {
+      FocusScope.of(Get.context!).unfocus();
+      formKey.currentState?.save();
+      if (formKey.currentState!.validate()) {
+        final response = await ApiProvider.instance().post(Endpoint.login,
+            data: dio.FormData.fromMap(
+                { "username": usernameController.text.toString(),
+              "password": passwordController.text.toString()
+            }));
+        if (response.statusCode == 200) {
+          await StorageProvider.write(StorageKey.status, "logged");
+          Get.offAllNamed(Routes.HOME);
+        } else {
+          Get.snackbar("Sorry", "Login Gagal", backgroundColor: Colors.orange);
+        }
+      }
+      loadingLogin(false);
+    } on dio.DioException catch (e) {
+      loadingLogin(false);
+      Get.snackbar("Sorry", e.message ?? "", backgroundColor: Colors.red);
+    } catch (e) {
+      loadingLogin(false);
+      Get.snackbar("Error", e.toString(), backgroundColor: Colors.red);
+      throw Exception('Error: $e');
+    }
+  }
+}
